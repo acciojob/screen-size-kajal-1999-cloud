2c9c6a0ed8287e84ad3170a3657b9688e11230e8
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-09 17:48:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const screenSize = document.createElement("h1") ;
+
+window.onResize =  function () {
+
+	screenSize.innerText = `width: ${window.innerwidth} and height: ${window.innerheight}` ;
+}
