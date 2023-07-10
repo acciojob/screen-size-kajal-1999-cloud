6f8212dd0abaa37c1701b906420ffaec36c57c8e
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-10 15:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 //your JS code here. If required.
-const screenSize = document.getElementById("sizeInfo") ;
-const heading = screenSize.firstElementChild;
+// const screenSize = document.getElementById("sizeInfo") ;
+// const heading = screenSize.firstElementChild;
 
-document.body.append(screenSize) ;
+// document.body.append(screenSize) ;
 
 // window.onresize =  function() {
 // 	screenSize.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}` ;
 // }
+
+//your JS code here. If required.
+const div = document.getElementById("sizeInfo");
+const heading = div.firstElementChild;
+window.onload = function(){
+	heading.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
 window.onresize = function(){
 	heading.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
 } 
+
+   
